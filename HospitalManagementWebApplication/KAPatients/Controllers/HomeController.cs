a20a7d703ebdf6253045e3cfaa59fe0150d91bfa
kappaswamy0195 <58642294+kappaswamy0195@users.noreply.github.com>
2020-01-27 06:26:46 -0500

    Hospital Mgmt Project added

---
+++ HospitalManagementWebApplication/KAPatients/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KAPatients.Models;
+
+namespace KAPatients.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Returns the view for index
+        public IActionResult Index()
+        {
+         
+            return View();
+        }
+        //Returns the view for privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //action to show the error message
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
