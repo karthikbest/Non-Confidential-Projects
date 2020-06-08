565c3482d92dcc6771c874d214b546368c37141d
kappaswamy0195 <58642294+kappaswamy0195@users.noreply.github.com>
2020-06-08 10:13:02 -0400

    Number Guessing Game moved to public repos
    Number Guessing Game moved to public repos

---
+++ Number Guesser WPF C# Application/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NumberGuessingGame
+{
+    public partial class MainWindow : Window
+    {
+        
+        VM vm = new VM();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Play();
+        }
+
+        private void TryButton_Click(object sender, RoutedEventArgs e)
+        {
+            vm.HandleAnswer();
+        }
+
+  
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+    }
+}
