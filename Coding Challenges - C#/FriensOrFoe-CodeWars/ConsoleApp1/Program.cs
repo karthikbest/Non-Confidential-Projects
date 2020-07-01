19a39fe209ebdfaee3a967e483e340c71a549232
kappaswamy0195 <58642294+kappaswamy0195@users.noreply.github.com>
2020-07-01 01:26:49 -0400

    Coding challenge from codewars solved

---
+++ Coding Challenges - C#/FriensOrFoe-CodeWars/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+Coding Challenge from Codewars.com
+
+SOLVED BY KARTHIK APPASWAMY
+.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingChallengeFromCodeWars
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] inputNames = new string[3] {
+                "Ryan", "Kieran", "Mark"
+            };
+            var output = FriendOrFoe(inputNames);
+            foreach (var o in output)
+                Console.WriteLine(o);
+            Console.ReadLine();
+        }
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            return names.Where(x=>x.Length==4);
+        }
+    }
+}
+
